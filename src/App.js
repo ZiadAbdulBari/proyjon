057275f5254542f2b29ecd2e7c9476e4ab617b2e
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-05-09 06:20:29 +0600

    product done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Cart from './components/Cart/Cart';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-4 gap-4 my-5'>
+          <Cart/>
+        </div>
+      </div>
     </div>
   );
 }
